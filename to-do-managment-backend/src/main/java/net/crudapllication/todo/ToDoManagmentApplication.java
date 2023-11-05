1b1a8b7649f9639c1647af854495bfea2682028c
beyzasenoll <beyzasenol08@gmail.com>
2023-11-05 21:16:11 +0300

    added and used mapper library

--- to-do-managment-backend/src/main/java/net/crudapllication/todo/ToDoManagmentApplication.java
+++ to-do-managment-backend/src/main/java/net/crudapllication/todo/ToDoManagmentApplication.java
@@ -1,10 +1,19 @@
 package net.crudapllication.todo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
 public class ToDoManagmentApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToDoManagmentApplication.class, args);
