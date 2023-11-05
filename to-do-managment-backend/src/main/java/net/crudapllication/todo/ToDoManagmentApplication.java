73173bb0304838241aa244d14338950d02cee523
beyzasenoll <beyzasenol08@gmail.com>
2023-11-05 17:04:44 +0300

    init commit

---
+++ to-do-managment-backend/src/main/java/net/crudapllication/todo/ToDoManagmentApplication.java
@@ -0,0 +1,13 @@
+package net.crudapllication.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoManagmentApplication.class, args);
+	}
+
+}
---
+++ to-do-managment-backend/src/test/java/net/crudapllication/todo/ToDoManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.crudapllication.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
