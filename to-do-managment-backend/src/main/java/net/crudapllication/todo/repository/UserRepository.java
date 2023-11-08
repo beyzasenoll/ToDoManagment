761b6e23e659657b3bbec7ba1d591bf32f9deef0
beyzasenoll <beyzasenol08@gmail.com>
2023-11-09 00:51:30 +0300

    adding database authentication

---
+++ to-do-managment-backend/src/main/java/net/crudapllication/todo/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package net.crudapllication.todo.repository;
+
+
+import net.crudapllication.todo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByEmail(String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
+}
