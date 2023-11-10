8537065b1f689f5f849be401bb1fe2faa7bf2ae6
beyzasenoll <beyzasenol08@gmail.com>
2023-11-10 18:28:10 +0300

    DB config have been completed.

---
+++ to-do-managment-backend/src/main/java/net/crudapllication/todo/utils/PasswordEncoderImpl.java
@@ -0,0 +1,14 @@
+package net.crudapllication.todo.utils;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncoderImpl {
+
+    public static void main(String[] args) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(passwordEncoder.encode("beyza"));
+
+        System.out.println(passwordEncoder.encode("admin"));
+    }
+}
